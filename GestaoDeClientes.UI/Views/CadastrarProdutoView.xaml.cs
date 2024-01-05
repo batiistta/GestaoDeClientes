b73d6efe1c536fc7929a656edec6b34209c44eb1
Bruno Tito <bruno.tito@suporterenova.onmicrosoft.com>
2024-01-04 22:10:55 -0300

    implementado a criação de produto

--- GestaoDeClientes.UI/Views/CadastrarProdutoView.xaml.cs
+++ GestaoDeClientes.UI/Views/CadastrarProdutoView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GestaoDeClientes.Domain.Models;
+using GestaoDeClientes.Infra.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -45,9 +47,28 @@ namespace GestaoDeClientes.UI.Views
             }
         }
 
-        private void btnCadastar_Click(object sender, RoutedEventArgs e)
+        private async Task btnCadastar_Click(object sender, RoutedEventArgs e)
         {
+            Produto produto = new Produto();
+            produto.Nome = txtNomeProduto.Text;
+            produto.Descricao = txtDescricaoProduto.Text;
+            produto.ValorCompra = Convert.ToDecimal(txtValorCompraProduto.Text);
+            produto.ValorUnitario = Convert.ToDecimal(txtValorUnitarioProduto.Text);
+            produto.Quantidade = Convert.ToInt32(txtQuantidadeProduto.Text);
+            produto.Ativo = true;
 
+            try
+            {
+                ProdutoRepository produtoRepository = new ProdutoRepository();
+                await produtoRepository.AddAsync(produto);
+                MessageBox.Show("Produto cadastrado com sucesso!");
+                ProdutoView produtoView = new ProdutoView();
+                this.Content = produtoView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
