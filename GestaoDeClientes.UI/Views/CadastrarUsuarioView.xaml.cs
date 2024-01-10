70843d05ec509fcb22a5582b6f4e9240c76a3665
Bruno Tito <bruno.tito@suporterenova.onmicrosoft.com>
2024-01-09 23:25:51 -0300

    implementado cadastro de usuario

--- GestaoDeClientes.UI/Views/CadastrarUsuarioView.xaml.cs
+++ GestaoDeClientes.UI/Views/CadastrarUsuarioView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GestaoDeClientes.Domain.Models;
+using GestaoDeClientes.Infra.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,42 @@ namespace GestaoDeClientes.UI.Views
         public CadastrarUsuarioView()
         {
             InitializeComponent();
+        }
+
+        private void btnCadastrarUsuario_Click(object sender, RoutedEventArgs e)
+        {
+            if (!txtNome.Text.Any() || !txtLogin.Text.Any() ||
+                !txtSenha.Text.Any() || !txtEmail.Text.Any())
+            {
+                MessageBox.Show("Por favor, preencha todos os campos!", "Atenção", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+
+            else
+            {
+                Usuario usuario = new Usuario();
+                usuario.Id = Guid.NewGuid().ToString();
+                usuario.Nome = txtNome.Text;
+                usuario.Login = txtLogin.Text;
+                usuario.Senha = txtSenha.Text;
+                usuario.Email = txtEmail.Text;
+                usuario.DataCadastro = DateTime.Now;
+
+                try
+                {
+                    UsuarioRepository usuarioRepository = new UsuarioRepository();
+                    usuarioRepository.AddAsync(usuario);
+                    MessageBox.Show("Usuário cadastrado com sucesso!", "Sucesso", MessageBoxButton.OK, MessageBoxImage.Information);
+                    UsuarioView usuarioView = new UsuarioView();
+                    this.Content = usuarioView;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
         }
     }
 }
