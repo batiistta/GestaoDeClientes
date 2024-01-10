d4d466cd21df81ebda843c75780de9672ce07b9f
Bruno Tito <bruno.tito@suporterenova.onmicrosoft.com>
2024-01-09 21:45:50 -0300

    adicionado criação da tabela de usuario

--- GestaoDeClientes.Infra/SQLs/DbSql.cs
+++ GestaoDeClientes.Infra/SQLs/DbSql.cs
@@ -9,37 +9,44 @@ namespace GestaoDeClientes.Infra.SQLs
     public static class DbSql
     {
         public static string CreateDatabase = @"
-            CREATE TABLE IF NOT EXISTS Cliente(
-                Id TEXT PRIMARY KEY,
-                Nome VARCHAR(200) NOT NULL ,
-                Email VARCHAR(200) ,
-                Telefone VARCHAR(30) ,
-                DataNascimento DATETIME ,
-                DataCadastro DATETIME ,
-                Ativo BIT 
-            );
-            
-            CREATE TABLE IF NOT EXISTS Produto(
-                Id TEXT PRIMARY KEY,
-                Nome VARCHAR(200) NOT NULL ,
-                Descricao VARCHAR(200) ,
-                ValorCompra DECIMAL(10,2) ,
-                ValorUnitario DECIMAL(10,2) ,
-                Quantidade INT,
-                Ativo BIT 
-            );
-            
-            CREATE TABLE IF NOT EXISTS Agendamento(
-                Id TEXT PRIMARY KEY,
-                IdCliente TEXT NOT NULL,
-                DataAgendamento DATETIME NOT NULL ,
-                ValorTotal DECIMAL(10,2) NOT NULL ,
-                ValorCusto DECIMAL(10,2) NOT NULL ,
-                Observacao VARCHAR(200) ,   
-                Lucro DECIMAL(10, 2) NOT NULL,
-                Ativo BIT 
-            );"
-        ;
+        CREATE TABLE IF NOT EXISTS Cliente(
+            Id TEXT PRIMARY KEY,
+            Nome VARCHAR(200) NOT NULL ,
+            Email VARCHAR(200) ,
+            Telefone VARCHAR(30) ,
+            DataNascimento DATETIME ,
+            DataCadastro DATETIME ,
+            Ativo BIT 
+        );
+        
+        CREATE TABLE IF NOT EXISTS Produto(
+            Id TEXT PRIMARY KEY,
+            Nome VARCHAR(200) NOT NULL ,
+            Descricao VARCHAR(200) ,
+            ValorCompra DECIMAL(10,2) ,
+            ValorUnitario DECIMAL(10,2) ,
+            Quantidade INT,
+            Ativo BIT 
+        );
 
-    }
+        CREATE TABLE IF NOT EXISTS Usuario(
+            Id TEXT PRIMARY KEY,
+            Login VARCHAR(100) NOT NULL,
+            Senha VARCHAR(100) NOT NULL,
+            Nome VARCHAR(200),
+            Email VARCHAR(200),
+            DataCadastro DATETIME
+        );
+        
+        CREATE TABLE IF NOT EXISTS Agendamento(
+            Id TEXT PRIMARY KEY,
+            IdCliente TEXT NOT NULL,
+            DataAgendamento DATETIME NOT NULL ,
+            ValorTotal DECIMAL(10,2) NOT NULL ,
+            ValorCusto DECIMAL(10,2) NOT NULL ,
+            Observacao VARCHAR(200) ,   
+            Lucro DECIMAL(10, 2) NOT NULL,
+            Ativo BIT 
+        );"
+    ;}
 }
